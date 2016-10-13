f2137be62aa2deb28ea6a9dd433478cc4cf575aa
Rama Olivera Fedi <rramiro.o@hotmail.com>
2016-10-12 23:00:49 -0300

    XML RSS feed endpoint

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 "use strict"; 
 const Messenger = require('./Messenger');
 const app = express();
+const main = new Messenger();
+
 
 app.listen(process.env.PORT || 3000);
 app.get('/', (req, res) => res.send("Piracy is illegal"));
+app.get('/rss', (req, res) => res.send(main.rss.xml({ indent: true })));
 
 // Program execution
-(() => new Messenger().listen())();
+(() => main.listen())();
